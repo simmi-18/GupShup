e49b8954dd691e9c0ab873c960b07122388be3ce
simmi-18 <simmisaini1803@gmail.com>
2026-04-08 17:53:54 +0530

    changes in knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,9 +14,10 @@ module.exports = {
   },
 
   production: {
-    client: process.env.DB_CLIENT,
-    connection: process.env.DATABASE_URL || {
+    client: process.env.DB_CLIENT || "mysql2",
+    connection: {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
